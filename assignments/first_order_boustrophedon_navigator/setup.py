ff03ad3895f06604cecba5c7ffec5ab2cd78873a
Dennis Liu <dennis48161025@gmail.com>
2025-01-27 02:52:46 +0800

    toolchain: remove scripts
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>
    
    The purpose of `scripts` is to ensure that executables are correctly
    placed in `install/<package>/lib`. By default, `setuptools` places the
    executables corresponding to `entry_points` in `install/<package>/bin`.
    By adding a `setup.cfg` file, you can control the behavior of
    `setuptools` and force the executables to be placed in
    `install/<package>/lib`, which is also the default location where ROS 2
    looks for them.

--- assignments/first_order_boustrophedon_navigator/setup.py
+++ assignments/first_order_boustrophedon_navigator/setup.py
@@ -6,21 +6,20 @@ package_name = 'first_order_boustrophedon_navigator'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('lib', package_name), glob('scripts/*'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='jdas',
-    maintainer_email='jnaneshwar.das@gmail.com',
+    maintainer='jdas, Dennis Liu', 
+    maintainer_email='jnaneshwar.das@gmail.com, dennis48161025@gmail.com',
     description='First-order boustrophedon navigation with PD control',
-    license='TODO: License declaration',
+    license='CC0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
