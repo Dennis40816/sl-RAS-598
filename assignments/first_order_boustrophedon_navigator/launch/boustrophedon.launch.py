ca00d192d091d3652d79df4f5cf4f9ec715b825b
Dennis Liu <dennis48161025@gmail.com>
2025-01-27 20:24:44 +0800

    feat: update controller
    - allow user use parameter to modify gain
    - add performance class to summary control result
    
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- assignments/first_order_boustrophedon_navigator/launch/boustrophedon.launch.py
+++ assignments/first_order_boustrophedon_navigator/launch/boustrophedon.launch.py
@@ -1,19 +1,65 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, Shutdown, LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch_ros.parameter_descriptions import ParameterValue
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        # Start the turtlesim node
-        Node(
-            package='turtlesim',
-            executable='turtlesim_node',
-            name='turtlesim'
+    controller_gains_arg = DeclareLaunchArgument(
+        'controller_gains',
+        default_value='{"Kp_linear": 10.0, "Kd_linear": 0.1, "Kp_angular": 5.0, "Kd_angular": 0.2}',
+        description='Controller gains for the boustrophedon controller'
+    )
+
+    close_all_after_finished_arg = DeclareLaunchArgument(
+        'close_all_after_finished',
+        default_value='true',
+        description='Shutdown all nodes after controller finishes'
+    )
+
+    controller_node = Node(
+        package='first_order_boustrophedon_navigator',
+        executable='boustrophedon_controller',
+        name='boustrophedon_controller',
+        # Need explicitly declare parameter type(It's dict which is not allowed)
+        parameters=[{
+            'controller_gains': ParameterValue(
+                LaunchConfiguration('controller_gains'),
+                value_type=str
+            )
+        }]
+    )
+
+    turtlesim_node = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        name='turtlesim',
+    )
+
+    on_controller_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=controller_node,
+            on_exit=[
+                LogInfo(
+                    msg=["Controller node finished. Shutting down all nodes..."]),
+                Shutdown(reason='Controller finished and shutting down all nodes')
+            ],
         ),
+        condition=IfCondition(LaunchConfiguration('close_all_after_finished'))
+    )
+
+    return LaunchDescription([
+        # declare args to ros2 param
+        controller_gains_arg,
+        close_all_after_finished_arg,
         
-        # Start our boustrophedon controller
-        Node(
-            package='first_order_boustrophedon_navigator',
-            executable='boustrophedon_controller',
-            name='boustrophedon_controller'
-        )
-    ]) 
+        # start nodes
+        controller_node,
+        turtlesim_node,
+        
+        # register events
+        on_controller_exit
+    ])
